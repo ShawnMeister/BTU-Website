50aabdea5129035f42efb17c28c9d738348d4606
ShawnMeister <shawn.mountenay@gmail.com>
2020-06-13 22:54:10 -0700

    test deploy without deploy.sh

--- vue-btu/src/views/Home.vue
+++ vue-btu/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="NINA moderately likes and may love in the future THE Vue.js App" />
+    <HelloWorld msg="10:53 pm" />
   </div>
 </template>
 
