745719c837863fcfa1b538f989f67936a515c442
ShawnMeister <shawn.mountenay@gmail.com>
2020-06-20 01:54:35 -0700

    new lights
    new light sources introduced, background colour change.

--- vue-btu/src/views/Enter/Enter.vue
+++ vue-btu/src/views/Enter/Enter.vue
@@ -19,7 +19,7 @@ var camera = new THREE.PerspectiveCamera(
 );
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setClearColor("black");
+renderer.setClearColor(0x0c000a);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -30,22 +30,40 @@ window.addEventListener("resize", () => {
   camera.updateProjectMatrix();
 });
 
-var geometry = new THREE.SphereGeometry(0, 10, 32);
-var material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-var sphere = new THREE.Mesh(geometry, material);
+var sphereGeometry = new THREE.SphereGeometry(0, 10, 32);
+var cylinderGeometry = new THREE.CylinderGeometry(1, 1, 1, 64);
+var greenMaterial = new THREE.MeshLambertMaterial({ color: 0x999999 });
+var grayMaterial = new THREE.MeshLambertMaterial({ color: 0x999999 });
+var sphere = new THREE.Mesh(sphereGeometry, greenMaterial);
+var cylinder = new THREE.Mesh(cylinderGeometry, grayMaterial);
 scene.add(sphere);
+scene.add(cylinder);
 
 camera.position.z = 5;
-var light = new THREE.PointLight(0xffffff, 1, 500);
-light.position.set(10, 0, 25);
-scene.add(light);
+
+const magenta = 0xff00ff;
+const cyan = 0x00ffff;
+
+var topLight = new THREE.DirectionalLight(cyan, 0.5);
+topLight.position.set(10, 25, 25);
+var rightLight = new THREE.DirectionalLight(magenta, 0.5);
+rightLight.position.set(25, 15, 25);
+var backLight = new THREE.DirectionalLight(0xffffff, 1);
+backLight.position.set(-25, -25, -100);
+var frontLight = new THREE.DirectionalLight(0xffffff, 0.5);
+frontLight.position.set(0, 10, 250);
+scene.add(topLight);
+scene.add(rightLight);
+scene.add(backLight);
+scene.add(frontLight);
 
 var flag = true;
 
 function animate() {
   requestAnimationFrame(animate);
   sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.02;
+  sphere.rotation.y += 0.05;
+  cylinder.rotation.x += 0.01;
 
   if (flag == false) {
     sphere.position.x -= 0.02;
