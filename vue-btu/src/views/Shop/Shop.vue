5aad8aa32e04e5caf97ade042fddba6530cf987d
ShawnMeister <shawn.mountenay@gmail.com>
2020-07-16 15:26:35 -0700

    Update Shop.vue

--- vue-btu/src/views/Shop/Shop.vue
+++ vue-btu/src/views/Shop/Shop.vue
@@ -4,20 +4,6 @@
 
     <div class="headline">Break The Universe</div>
 
-    <!-- <div v-if="showShirtsFlag===false">
-      <button v-on:click="showShirtsFlag=true;">CLICK ME TO SHOW SHIRTS</button>
-    </div>
-    <div v-if="showShirtsFlag===true">
-      <button v-on:click="showShirtsFlag=false;">CLICK ME TO SHOW SHIRTS</button>
-    </div>
-    <div id="merch-grid" v-show="showShirtsFlag===true">
-      <div v-for="n  in 3 " :key="n" class="shirt">
-        <h3>asdf</h3>
-        {{msg}}
-        <img src="@/assets/btu-standard-shirt.png" alt />
-      </div>
-    </div>-->
-
     <div>
       <button v-on:click="showShirtsFlag = !showShirtsFlag">CLICK ME TO SHOW SHIRTS</button>
     </div>
