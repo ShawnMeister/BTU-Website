d7ce88cdd64ac46fb67296458956d9d675a4d10d
ShawnMeister <shawn.mountenay@gmail.com>
2020-07-16 15:26:12 -0700

    Update Shop.vue

--- vue-btu/src/views/Shop/Shop.vue
+++ vue-btu/src/views/Shop/Shop.vue
@@ -4,10 +4,28 @@
 
     <div class="headline">Break The Universe</div>
 
-    <div id="merch-grid">
-      <div v-for="n  in 99 " :key="n" class="shirt">
+    <!-- <div v-if="showShirtsFlag===false">
+      <button v-on:click="showShirtsFlag=true;">CLICK ME TO SHOW SHIRTS</button>
+    </div>
+    <div v-if="showShirtsFlag===true">
+      <button v-on:click="showShirtsFlag=false;">CLICK ME TO SHOW SHIRTS</button>
+    </div>
+    <div id="merch-grid" v-show="showShirtsFlag===true">
+      <div v-for="n  in 3 " :key="n" class="shirt">
         <h3>asdf</h3>
         {{msg}}
+        <img src="@/assets/btu-standard-shirt.png" alt />
+      </div>
+    </div>-->
+
+    <div>
+      <button v-on:click="showShirtsFlag = !showShirtsFlag">CLICK ME TO SHOW SHIRTS</button>
+    </div>
+
+    <div id="merch-grid" v-show="showShirtsFlag">
+      <div v-for="n  in 3 " :key="n" class="shirt">
+        <h3>Sexy Shirt</h3>
+        <p>{{msg}}</p>
         <img src="@/assets/btu-standard-shirt.png" alt />
       </div>
     </div>
@@ -24,7 +42,7 @@
 
 export default {
   data: function() {
-    return { msg: "dude really?" };
+    return { msg: "dude really?", showShirtsFlag: false };
   }
 };
 </script>
