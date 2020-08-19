f323d3605d0ba0e2019163b5fcdcbdfbf3b6daa9
ShawnMeister <shawn.mountenay@gmail.com>
2020-08-19 05:49:57 -0700

    rockin atlas

--- api/DB.ts
+++ api/DB.ts
@@ -1,3 +1,8 @@
 module.exports = {
-  DB: "mongodb://localhost:27017/mevncrud",
+  //to run locally use this
+  // DB: "mongodb://localhost:27017/mevncrud", 
+
+  //to run on atlas use this:
+  DB: "mongodb+srv://admin:Al3IDesE7rlhUIOC@cluster-btu.uplrc.gcp.mongodb.net/mevncrud",
+
 };
